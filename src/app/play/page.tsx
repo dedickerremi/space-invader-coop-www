225574dd95b242c9bf212fa4d2e632770dec0daf
Dedicker Remi <remi@reverseddigital.com>
2026-02-18 19:50:01 +0100

    debug

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -52,12 +52,18 @@ export default function PlayPage() {
   }
 
   return (
-    <GameCanvas
-      matchToken={matchData.matchToken}
-      wsUrl={matchData.wsUrl}
-      matchId={matchData.matchId}
-      playerId={matchData.playerId}
-    />
+    <>
+      <GameCanvas
+        matchToken={matchData.matchToken}
+        wsUrl={matchData.wsUrl}
+        matchId={matchData.matchId}
+        playerId={matchData.playerId}
+      />
+      {/* Debug: vérifier l’URL WS utilisée (à retirer après diagnostic) */}
+      <div style={{ position: 'fixed', bottom: '2rem', left: 8, fontSize: 10, color: '#333', fontFamily: 'monospace' }}>
+        WS: {matchData.wsUrl}
+      </div>
+    </>
   )
 }
 
